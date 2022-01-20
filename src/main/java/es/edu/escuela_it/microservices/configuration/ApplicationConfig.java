d7cd4ae8d627b0957efca1f30979023f018f0970
christianvilca <christian.vilcaq@gmail.com>
2022-01-20 16:56:19 -0500

    feat: externalizar II

--- src/main/java/es/edu/escuela_it/microservices/configuration/ApplicationConfig.java
+++ src/main/java/es/edu/escuela_it/microservices/configuration/ApplicationConfig.java
@@ -3,22 +3,20 @@ package es.edu.escuela_it.microservices.configuration;
 import lombok.Data;
 import lombok.ToString;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @Data
 @ToString
+@ConfigurationProperties(prefix = "app")
 public class ApplicationConfig {
 
-    @Value("${app.name}")
     private String name;
 
-    @Value("${app.year}")
     private Integer year;
 
-    @Value("${app.edition}")
     private String edition;
 
-    @Value("${app.countries}")
     private String[] countries;
 }
