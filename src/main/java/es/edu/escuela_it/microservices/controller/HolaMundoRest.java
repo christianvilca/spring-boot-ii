758ec9816ab4b14fb3a38ec686775da42cebeb8f
christianvilca <christian.vilcaq@gmail.com>
2022-03-31 18:24:51 -0500

    feat: Log Propio

--- src/main/java/es/edu/escuela_it/microservices/controller/HolaMundoRest.java
+++ src/main/java/es/edu/escuela_it/microservices/controller/HolaMundoRest.java
@@ -1,6 +1,8 @@
 package es.edu.escuela_it.microservices.controller;
 
 import es.edu.escuela_it.microservices.configuration.ApplicationConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,13 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
 //@PropertySource(value = "classpath:mensajes.properties") // Properties de archivo especifico
 public class HolaMundoRest {
 
+    // Recomendable usar slf4j ya que trabajamos con cualquier nivel de traza
+    private Logger log = LoggerFactory.getLogger(HolaMundoRest.class);
+
     @Autowired
     private ApplicationConfig appConfig;
 
     @GetMapping("holamundo")
     public String saludo(){
 
-        System.out.println(appConfig.toString()); // ApplicationConfig(name=Microservice Java Escuela IT, year=2020, edition=Community, countries=[es, ar, it, ca, br])
+        log.trace("Ejecutando hola mundo trace");
+        log.debug("Ejecutando hola mundo debug");
+        log.info("Ejecutando hola mundo info");
+        log.warn("Ejecutando hola mundo warn");
+        log.error("Ejecutando hola mundo error");
+
+        //System.out.println(appConfig.toString()); // ApplicationConfig(name=Microservice Java Escuela IT, year=2020, edition=Community, countries=[es, ar, it, ca, br])
         return "Hola mundo Servicio rest Java";
     }
 }
