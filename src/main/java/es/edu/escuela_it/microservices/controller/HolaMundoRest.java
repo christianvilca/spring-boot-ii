1d88f13317dc9e1d83ec3add26d368bfb8702a94
christianvilca <christian.vilcaq@gmail.com>
2022-03-31 19:06:03 -0500

    feat: Lombok @Slf4j

--- src/main/java/es/edu/escuela_it/microservices/controller/HolaMundoRest.java
+++ src/main/java/es/edu/escuela_it/microservices/controller/HolaMundoRest.java
@@ -1,6 +1,7 @@
 package es.edu.escuela_it.microservices.controller;
 
 import es.edu.escuela_it.microservices.configuration.ApplicationConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 //@PropertySource(value = "classpath:mensajes.properties") // Properties de archivo especifico
+@Slf4j
 public class HolaMundoRest {
 
     // Recomendable usar slf4j ya que trabajamos con cualquier nivel de traza
-    private Logger log = LoggerFactory.getLogger(HolaMundoRest.class);
+    // Con @Slf4j ya no hace falta esta linea, y esta disponible la variable "log"
+    //private final Logger log = LoggerFactory.getLogger(HolaMundoRest.class);
 
     @Autowired
     private ApplicationConfig appConfig;
